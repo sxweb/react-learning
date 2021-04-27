964ec846a43e43be8474849cd0ab8adb5f95f628
Yuriy <phpred@gmail.com>
2021-04-27 06:12:29 +0300

    try to create simple react fragments

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function Person({name, surname, link}){
+  return(
+    <>
+      <p>The persons name is {name}, surname - {surname}</p>
+      Social link - <a href={link}>{name} {surname}</a>
+    </>
+  )
+}
+
+function PrintPersons(){
+  return (
+    <>
+      <Person name="Yuriy" surname="Vozhagov" link="google.com"/>
+      <Person name="Sam" surname="Jason" link="google.com"/>
+      <Person name="Ann" surname="Smith" link="google.com"/>
+    </>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PrintPersons />
   </React.StrictMode>,
   document.getElementById('root')
 );
