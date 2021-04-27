fe1b89f7f77c9eb0397f1b1bf517af4055be13a7
Yuriy <phpred@gmail.com>
2021-04-27 19:49:58 +0300

    created project structure

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './components/app'
 import reportWebVitals from './reportWebVitals';
 
 function Person({name, surname, link}){
@@ -25,12 +25,9 @@ function PrintPersons(){
 
 ReactDOM.render(
   <React.StrictMode>
-    <PrintPersons />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
