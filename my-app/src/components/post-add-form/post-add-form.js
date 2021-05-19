007507d4ef9bb1c32e2b85a67794e1e09480579b
Yuriy <phpred@gmail.com>
2021-05-19 06:40:52 +0300

    add item function, rewrote post add form from function to class

--- my-app/src/components/post-add-form/post-add-form.js
+++ my-app/src/components/post-add-form/post-add-form.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './post-add-form.css';
 
-const PostAddForm = ({addItem})=>{
-    return(
-        <div className="bottom-panel d-flex">
-            <input 
-                type="text"
-                placeholder="type your message"
-                className="form-control new-post-label"
-            />
-            <button
-                type="submit"
-                className="btn btn-outline-secondary"
-                onClick = {() => addItem('hello')}>
-            Add</button>
-        </div>
-    )
+export default class PostAddForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    
+
+    onValueChange(e){
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        this.props.addItem(this.state.text);
+        this.setState({
+            text: ''
+        })
+    }
+
+    render(){
+        return(
+            <form
+                className="bottom-panel d-flex"
+                onSubmit={this.onSubmit}>
+                <input 
+                    type="text"
+                    placeholder="type your message"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value = {this.state.text}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-outline-secondary">
+                Add</button>
+            </form>
+        )
+    }
 }
 
-export default PostAddForm;
